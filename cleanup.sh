eab811777aec90a15ba9c0fab6d34a347970949a
Shoaib <35446260+shoaib42@users.noreply.github.com>
2024-10-29 11:27:57 -0400

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -8,13 +8,15 @@ SUBDOMAIN="_acme-challenge"
 
 # Remove a DNS TXT record
 remove_txt_record() {
-    RECORD_ID=$(curl -s \
+    RECORD_IDS=$(curl -s \
                 "https://www.namesilo.com/api/dnsListRecords?version=1&key=$API_KEY&type=json&domain=$DOMAIN" \
                 | jq -r ".reply.resource_record.[] | select(.host==\"$SUBDOMAIN.$DOMAIN\").record_id")
 
-    if [ -n "$RECORD_ID" ]; then
-        curl -s "https://www.namesilo.com/api/dnsDeleteRecord?version=1&key=$API_KEY&type=xml&domain=$DOMAIN&rrid=$RECORD_ID" \
-             && echo "Successfully removed TXT record with ID $RECORD_ID"
+    if [ -n "$RECORD_IDS" ]; then
+        for RECORD_ID in $RECORD_IDS ; do
+            curl -s "https://www.namesilo.com/api/dnsDeleteRecord?version=1&key=$API_KEY&type=xml&domain=$DOMAIN&rrid=$RECORD_ID" \
+                 && echo "Successfully removed TXT record with ID $RECORD_ID"
+        done
     else
         echo "Record ID not found for $SUBDOMAIN.$DOMAIN; nothing to remove."
     fi
